1796ab5ee909fc919a3c68eb15b2585e7ff602bf
Pablo <lcunha.pablo@gmail.com>
2022-10-19 16:28:16 -0300

    Funcionalidade pronta

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -13,20 +13,53 @@
         required autocomplete="off">
         </b-form-textarea>
       </b-form-group>
-      <b-button type="submite" variant="outline-primary">Salvar</b-button>
+      <b-button type="submite" variant="outline-primary" @click="saveTask">Salvar</b-button>
     </b-form>
   </div>
 </template>
 
 <script>
+import ToastMixin from "@/mixins/ToastMixin.js"
 export default {
   name: "Form",
+  mixins: [ToastMixin],
+
   data() {
     return {
       form: {
         subject: "",
         description: ""
+      },
+      methodSave: "new"
+    }
+  },
+  created() {
+    if(this.$route.params.index === 0 || this.$route.params.index != undefined) {
+      this.methodSave = "update"
+      let tasks = JSON.parse(localStorage.getItem("tasks"))
+      this.form = tasks[this.$route.params.index]
+    }
+  },
+  methods: {
+    saveTask() {
+      // Verificando se é uma tarefa que será atualizada, ou uma tarefa nova
+      if(this.methodSave === "update") {
+        let tasks = JSON.parse(localStorage.getItem("tasks"))
+        tasks[this.$route.params.index] = this.form
+        localStorage.setItem("tasks", JSON.stringify(tasks))
+        this.showToast("success", "Sucesso!", "Tarefa atualizada.")
+        this.$router.push({name: "list"})
+        return
       }
+      // Testando se existem tarefas no localStorage
+      let tasks = (localStorage.getItem("tasks")) ? JSON.parse(localStorage.getItem("tasks")) : []
+      // Adicionando task ao array de tarefas
+      tasks.push(this.form)
+      // Definindo array dentro do localStorage e exibindo mensagem de sucesso
+      localStorage.setItem("tasks", JSON.stringify(tasks))
+      this.showToast("success", "Sucesso!", "Tarefa criada.")
+      // Após salvar task, leva o usuário à lista de tarefas
+      this.$router.push({name: "list"})
     }
   }
 }
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,9 +1,57 @@
 <template>
   <div class="container mt-2">
-    <b-card title="Titulo">
-      <b-card-text>descrição</b-card-text>
-      <b-button variant="outline-secondary" class="mr-2">Editar</b-button>
-      <b-button variant="outline-danger" class="mr-2">Excluir</b-button>
-    </b-card>
+    <div v-for="(task, index) in tasks" :key="index">
+      <b-card :title="task.subject" class="mb-2">
+        <b-card-text>{{ task.description }}</b-card-text>
+        <b-button variant="outline-secondary" class="mr-2" @click="editTask(index)">Editar</b-button>
+        <b-button variant="outline-danger" class="mr-2" @click="deleteTask(task, index)">Excluir</b-button>
+      </b-card>
+    </div>
+    <div>
+      <b-modal ref="modalDelete" hide-footer title="Deletar Tarefa">
+        <div class="d-block text-center">
+          Deseja deletar a tarefa "{{ taskSelected.subject }}"?
+        </div>
+        <div class="mt-3 d-flex justify-content-end">
+          <b-button variant="outline-secondary" class="mr-2" @click="hideModal">Cancelar</b-button>
+          <b-button variant="outline-danger" class="mr-2" @click="confirmDelete">Excluir</b-button>
+        </div>
+      </b-modal>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "List",
+  data() {
+    return {
+      tasks: [],
+      taskSelected: []
+    }
+  },
+  created() {
+    this.tasks = (localStorage.getItem("tasks")) ? JSON.parse(localStorage.getItem("tasks")) : []
+  },
+  methods: {
+    editTask(index) {
+      // Redireciona ao formulário, passando o index da tarefa como parametro
+      this.$router.push({ name: "form", params: { index }})
+    },
+    deleteTask(task, index) {
+      this.taskSelected = task
+      this.taskSelected.index = index
+      this.$refs.modalDelete.show()
+    },
+    hideModal() {
+      this.$refs.modalDelete.hide()
+    },
+    confirmDelete() {
+      this.tasks.splice(this.taskSelected.index, 1)
+      localStorage.setItem("tasks", JSON.stringify(this.tasks))
+      this.hideModal()
+    }
+  }
+}
+</script>
+
